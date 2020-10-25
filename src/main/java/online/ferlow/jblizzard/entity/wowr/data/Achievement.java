0ded22e9f05b310cfaf5a271041835417cba08fe
Ferlow <hello.flobauch@gmail.com>
2020-10-25 19:31:42 +0000

    achievement

--- src/main/java/online/ferlow/jblizzard/entity/wowr/data/Achievement.java
+++ src/main/java/online/ferlow/jblizzard/entity/wowr/data/Achievement.java
@@ -1,4 +1,8 @@
 package online.ferlow.jblizzard.entity.wowr.data;
 
 public class Achievement {
+    private String id, name, description;
+    private int points;
+    private boolean is_account_wide;
+    private AchievementCategory achievementCategory;
 }
